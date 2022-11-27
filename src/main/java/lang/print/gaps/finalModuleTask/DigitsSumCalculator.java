968b95b2f11cb921a5baf4329728963ef1facd4d
onisim_albarov <onisim93@gmail.com>
2022-11-27 09:30:33 +0400

    complete module3-taskFinal

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int sum = 0;
+        while (number != 0) {
+            int remainderOfDividing = number % 10;
+            number /= 10;
+            sum += remainderOfDividing;
+        }
+
+        System.out.println(sum);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,14 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        StringBuilder result = new StringBuilder();
+
+        while (number != 0) {
+            int remainderOfDividing = number % 10;
+            number /= 10;
+            result.append(remainderOfDividing);
+        }
+
+        System.out.println(result);
     }
 }
